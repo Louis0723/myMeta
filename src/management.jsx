ebfd60ded256541dab81ecfe77741a439eba6b3e
rorechen <cheng2004300@gmail.com>
2018-01-25 00:53:53 +0800

    新增GibberishAES用於AES256加解密(預設256)

--- src/management.jsx
+++ src/management.jsx
@@ -1,6 +1,8 @@
 
 import * as React from 'react';
 import { Form,Button }  from 'react-bootstrap';
+import * as GibberishAES from 'gibberish-aes/dist/gibberish-aes-1.0.0.min';
+
 export class ManagementComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -18,8 +20,16 @@ export class ManagementComponent extends React.Component {
   render() {
     return (
       <Form>
+        <Button onClick={this.doTestAES256Btn} >AES測試</Button>
         <Button>Save Account {/Electron/.test(navigator.userAgent)?'':'To This Browser'}</Button>
       </Form>
     );
   }
+
+    doTestAES256Btn() {
+        let enc = GibberishAES.enc("This sentence is super secret", "ultra-strong-password");
+        alert(enc);
+        let decEnc = GibberishAES.dec(enc, "ultra-strong-password");
+        alert(decEnc);
+    }
 }
