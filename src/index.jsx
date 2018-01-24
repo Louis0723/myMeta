61cab83e7f274a9339485e9d3076725db07268a9
Lin-Yi Tang <lursun914013@gmail.com>
2018-01-24 09:25:56 +0800

    yuhuirore

--- src/index.jsx
+++ src/index.jsx
@@ -3,21 +3,26 @@ import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { AppComponent } from './app';
 
-
-const render = (Component) => {
+if (ENV === 'production') {
     ReactDOM.render(
-        <AppContainer>
-            <Component />
-        </AppContainer>
+        <AppComponent />
         ,
         document.getElementById('root')
     );
+} else {
+    const render = (Component) => {
+        ReactDOM.render(
+            <AppContainer>
+                <Component />
+            </AppContainer>
+            ,
+            document.getElementById('root')
+        );
+    }
+    render(AppComponent);
+    if (module.hot) {
+        module.hot.accept('./app', () => { // ./app 必須與 app.jsx 同名
+            render(AppComponent);
+        })
+    };
 }
-
-render(AppComponent);
-
-if (module.hot) {
-    module.hot.accept('./app', () => { // ./app 必須與 app.jsx 同名
-        render(AppComponent);
-    })
-};
